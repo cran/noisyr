9d515bf3c8b8a65e4c7b0be2b3110fa33b33753e
Ilias Moutsopoulos <im383@cam.ac.uk>
2021-01-06 08:50:02 +0000

    version 0.1.0

---
+++ R/cast_gtf_to_genes.R
@@ -0,0 +1,44 @@
+#' Function to extract exon names and positions from a gtf file
+#' @description This function is used to extract all exons and their positions
+#' in the genome from an input gtf file.
+#' @param filename path to the gtf file
+#' @param feature the feature to extract from the last column of the gtf/gff file;
+#' default is exon
+#' @return Returns a tibble of the ids, names, chromosomes, start and end positions
+#' of each exon found in the gtf file. If refGenome is installed, that is used
+#' for the reading and is faster, otherwise the gtf is read manually with a warning
+#' @export
+#' @examples
+#' fl <- system.file("extdata", "example.gtf.gz", package="Rsamtools", mustWork=TRUE)
+#' genes <- cast_gtf_to_genes(fl)
+
+cast_gtf_to_genes = function(filename,
+                             feature="exon"){
+  genes <- tibble::as_tibble(utils::read.table(filename,
+                                                 sep="\t",
+                                                 stringsAsFactors = FALSE)) %>%
+      tibble::rowid_to_column(var="id")
+    base::colnames(genes) <- base::c("id", "seqid", "source", "feature", "start",
+                                     "end", "score", "strand", "frame", "attributes")
+    base::message("Done reading")
+
+    extract_attributes <- function(gtf_attributes, att_of_interest){
+      att <- base::strsplit(gtf_attributes, "; ")
+      att <- base::gsub("\"","",base::unlist(att))
+      if(!base::is.null(base::unlist(base::strsplit(
+        att[base::grep(att_of_interest, att)], " ")))){
+        return(base::unlist(base::strsplit(
+          att[base::grep(att_of_interest, att)], " "))[2])
+      }else{
+        return(NA)}
+    }
+    id=NULL; gene_id=NULL; seqid=NULL; start=NULL; end=NULL
+    genes <- genes %>% dplyr::filter(feature==feature)
+    genes <- genes %>% dplyr::mutate(
+      gene_id = base::unlist(base::lapply(genes$attributes,
+                                          extract_attributes,
+                                          "gene_id")))
+  genes <- genes %>%
+    dplyr::select(id, gene_id, seqid, start, end)
+  return(genes)
+}
---
+++ R/optimise_window_length.R
@@ -0,0 +1,99 @@
+#' Optimise the elements per window for the count matrix approach
+#' @description This function optimises the number of elements per window
+#' that is used in calculate_distance_matrices_counts(), by requiring
+#' the distribution of correlations/distances to stabilise to a
+#' uniform distribution. The Jensen-Shannon divergence is used to assess
+#' the stability.
+#' @param expression.matrix expression matrix, can be normalized or not
+#' @param method one of the correlation or distance metrics to be used,
+#' defaults to pearson correlation; list of all methods in
+#' get_methods_correlation_distance()
+#' @param winlen.min,winlen.max,winlen.by definition of the parameter search space
+#' @param nstep.frac an alternative way to specify the step size, as a fraction of
+#' the window length; default is 5\%
+#' @param iternum number of iterations for the subsampling and calculation of JSE;
+#' subsampling is needed because shorter windows have fewer points
+#' @param save.plot name of the pdf in which to print the output plot
+#' showing the distribution of JSE by window
+#' @return A single value of the optimal number of elements per window
+#' @export
+#' @examples optimise_window_length(
+#' matrix(1:100+runif(100), ncol=5, byrow=TRUE),
+#'   winlen.min=3, winlen.max=5, iternum=5)
+optimise_window_length = function(
+  expression.matrix,
+  method = "correlation_pearson",
+  winlen.min=NULL,
+  winlen.max=NULL,
+  winlen.by=NULL,
+  nstep.frac=0.05,
+  iternum=1000,
+  save.plot=NULL
+)
+{
+  nrows <- base::nrow(expression.matrix)
+
+  if(base::is.null(winlen.min)) winlen.min = base::max(base::floor(nrows * 0.01), 1)
+
+  if(base::is.null(winlen.max)) winlen.max = base::max(base::floor(nrows * 0.33), 1)
+
+  if(base::is.null(winlen.by)) winlen.by = base::max(base::floor(nrows * 0.01), 1)
+
+  winlens <- base::seq(from=winlen.min, to=winlen.max, by=winlen.by)
+
+  tbl <- tibble::tibble()
+  for(cnt in base::rev(base::seq_along(winlens))){
+
+    winlen <- winlens[cnt]
+    diff <- tibble::tibble(winlen=base::rep(winlen, iternum),
+                           iter=1:iternum,
+                           JSE=rep(NA_real_, iternum))
+    base::suppressMessages(
+      obj <- noisyr::calculate_distance_matrices_counts(expression.matrix=expression.matrix,
+                                                        n.elements.per.window=winlen,
+                                                        nstep.frac=nstep.frac,
+                                                        method=method)
+    )
+    rmeans <- base::rowMeans(obj$dist, na.rm=TRUE)
+    x=base::abs(rmeans[!base::is.na(rmeans)])
+    xlen <- base::length(x)
+    if(cnt == length(winlens)) subsize <- xlen
+    yy=seq(from=base::min(abs(rmeans), na.rm=TRUE),
+           to=base::max(rmeans, na.rm=TRUE),
+           length.out=subsize)
+
+    for(iter in base::seq_len(iternum)){
+      xx <- x[base::sort(base::sample(1:xlen, subsize))]
+      diff[iter, "JSE"] <- philentropy::jensen_shannon(xx, yy, testNA=FALSE, unit="log")
+    }
+
+    tbl <- base::rbind(diff, tbl)
+  }
+
+  JSE=NULL
+  p <- ggplot2::ggplot(tbl) +
+    ggplot2::geom_boxplot(ggplot2::aes(x=base::as.factor(winlen), y=JSE)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90)) +
+    ggplot2::xlab("Window length")
+  if(!base::is.null(save.plot)) grDevices::pdf(save.plot)
+  print(p)
+  if(!base::is.null(save.plot)) grDevices::dev.off()
+
+  dists <- base::list()
+  for(i in base::seq_along(winlens)){
+    dists[[i]] = dplyr::filter(tbl, winlen==winlens[i])$JSE
+  }
+
+  for(i in base::seq_len(base::length(winlens))){
+    x <- base::vector()
+    for(j in base::seq_len(base::length(winlens))){
+      if(i!=j){
+        x <- c(x, stats::t.test(dists[[i]], dists[[j]])$p.value < 0.05)
+      }
+    }
+    if(base::sum(!x)>1){
+      break
+    }
+  }
+  return(winlens[i])
+}
